3b42bcea09827bfa4bbb3f4873f2703141d68be9
Jonathan Chiang <jc91765@gmail.com>
2021-08-30 21:29:05 -0700

    set up blank entries object

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
+const weeklyEntries = {
+  monday: {},
+  tuesday: {},
+  wednesday: {},
+  thursday: {},
+  friday: {},
+  saturday: {},
+  sunday: {}
+};
+/* sample entry: {monday: {morning: true, 10: do work}} */
 const $addEntryButton = document.querySelector('#add-entry-button');
 const $addEntryBox = document.querySelector('#add-entry');
 const $saveEntryButton = document.querySelector('#save-entry');
+const $entryForm = document.querySelector('#entry-form');
 
 $addEntryButton.addEventListener('click', function (event) {
   $addEntryBox.className = '';
+  $entryForm.reset();
+
 });
 
 $saveEntryButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  weeklyEntries.monday.morning = true;
   $addEntryBox.className = 'hidden';
 });
