13cf623a453a2236676abcc2054620740bf4e116
Jonathan Chiang <jc91765@gmail.com>
2021-08-31 19:05:37 -0700

    revised entry object

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ const weeklyEntries = {
   saturday: {},
   sunday: {}
 };
+
 /* sample entry: {monday: {morning: true, 10: do work}} */
+
 const $addEntryButton = document.querySelector('#add-entry-button');
 const $addEntryBox = document.querySelector('#add-entry');
-const $saveEntryButton = document.querySelector('#save-entry');
 const $entryForm = document.querySelector('#entry-form');
 
 $addEntryButton.addEventListener('click', function (event) {
@@ -19,8 +20,28 @@ $addEntryButton.addEventListener('click', function (event) {
 
 });
 
-$saveEntryButton.addEventListener('click', function (event) {
+$entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  weeklyEntries.monday.morning = true;
+  const dayOfW = $entryForm.day.value;
+  const hour = $entryForm.hour.value;
+  const ampm = $entryForm.morning.value;
+  const time = `${hour}-${ampm}`;
+  weeklyEntries[dayOfW][time] = $entryForm.entry.value;
   $addEntryBox.className = 'hidden';
 });
+
+/* function submitForm(event) {
+  let currentEntry = null;
+  event.preventDefault();
+  if ($form.getAttribute('data-view') === 'entry-form') {
+    const filledForm = {};
+    filledForm.entryID = data.nextEntryId;
+    filledForm.title = $form.title.value;
+    filledForm.photourl = $form.photo.value;
+    filledForm.notes = $form.notes.value;
+    data.nextEntryId++;
+    $form.reset();
+    data.entries.push(filledForm);
+    document.querySelector('#photo').setAttribute('src', 'images/placeholder-image-square.jpg');
+    showEntries();
+  } */
